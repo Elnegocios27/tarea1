92b4d5fb74a9fce62ba10bef9a86494746d245a6
Kenn27C <maximogaspar777@outlook.com>
2025-08-25 14:26:43 -0300

    my first split of interface

---
+++ show_menu.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int show_menu()
+{
+    printf("=============================");
+    printf("  Welcome to your To do list. ");
+    printf("1. Add tasks.");
+    printf("2. Show tasks.");
+    printf("3. Tick tasks.");
+    printf("4. Delete tasks.");
+    printf("5. exit.");
+    printf("=============================");
+}
